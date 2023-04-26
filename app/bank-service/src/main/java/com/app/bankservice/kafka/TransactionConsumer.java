8b9769c52a3d06fb09953f9dfd7ee47731b31192
bruzzo12 <ruzzo.b@husky.neu.edu>
2023-04-25 22:56:06 -0400

    Initial MySQL connection with Kafka

--- app/bank-service/src/main/java/com/app/bankservice/kafka/TransactionConsumer.java
+++ app/bank-service/src/main/java/com/app/bankservice/kafka/TransactionConsumer.java
@@ -1,5 +1,7 @@
 package com.app.bankservice.kafka;
 
+import com.app.bankservice.kafka.entity.BankTransactionData;
+import com.app.bankservice.kafka.repository.repository.TransactionDataRepository;
 import com.app.basedomains.dto.TransactionEvent;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -10,18 +12,40 @@ import org.springframework.stereotype.Service;
 public class TransactionConsumer {
 
 	private static final Logger LOGGER = LoggerFactory.getLogger(TransactionConsumer.class);
+	private TransactionDataRepository dataRepository;
+
+	public TransactionConsumer(TransactionDataRepository dataRepository) {
+		this.dataRepository = dataRepository;
+	}
 
 	@KafkaListener(
 			topics = "${spring.kafka.topic.name}"
 			,groupId = "${spring.kafka.consumer.group-id}"
 	)
+
+	//The TransactionEvent will most likely be replaced with a custom event to fit our bank transaction properties
 	public void consume(TransactionEvent event){
 		LOGGER.info(String.format("Transaction event received in bank service => %s",
 				event.toString()));
 
+		//Create BankTransactionData object so we can then set the data from the TransactionEvent that is being consumed.
+		//Once we have the structure for the TransactionEvent we can actually parse it/get the individual
+		// data points such as sender, receiver, amount (basically all BankTransactionData properties)
+		//Setter code is place holder for now
+		BankTransactionData bankTransactionData = new BankTransactionData();
+		bankTransactionData.setSender(event);
+		bankTransactionData.setComments(event);
+		bankTransactionData.setAmount(event);
+		bankTransactionData.setRecipient(event);
+		bankTransactionData.setOperation(event);
+
+		//We then save the banktransaction entity to the MySQL database and it populates the table
+		/* TODO: save the transaction event into the database */
+		dataRepository.save(bankTransactionData);
+
 		/* TODO: If payment out, validate if account balance > amt */
 		// update transactionEvent status and message, populate the info as response for POST
-		/* TODO: save the transaction event into the database */
+
 		/* TODO: update account balance */
 	}
 }
